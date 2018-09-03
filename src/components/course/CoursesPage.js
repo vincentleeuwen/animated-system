39fa0209e82208014a2dce53164fe6223429e45e
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-03 14:43:57 +0200

    Add CouresList, CourseListRow

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseList from './CourseList';
 
 class CoursesPage extends Component {
   constructor(props, context) {
@@ -11,10 +12,11 @@ class CoursesPage extends Component {
     return <div key={index}>{course.title}</div>;
   }
   render() {
+    const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
-        {this.props.courses.map(this.courseRow)}
+        <CourseList courses={courses} />
       </div>
     );
   }
