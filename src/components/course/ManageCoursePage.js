7204ac08a971d680fd9531a00c49f25754068a2d
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-03 15:52:38 +0200

    course form

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -2,29 +2,45 @@ import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
+import CourseForm from './CourseForm';
 
 class ManageCoursePage extends Component {
   constructor(props, context) {
     super(props, context);
+
+    this.state = {
+      course: Object.assign({}, this.props.course),
+      errors: {}
+    };
   }
 
   render() {
     return (
-      <div>
-        <h1>Manage Course Page</h1>
-      </div>
+      <CourseForm
+        course={this.state.course}
+        errors={this.state.errors}
+        allAuthors={[]}
+      />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-
-}
+  course: PropTypes.object.isRequired
+};
 
 function mapStateToProps(state, ownProps) {
+  let course = {
+    id: '',
+    watchHref: '',
+    title: '',
+    authorId: '',
+    length: '',
+    category: ''
+  };
   return {
-    state: state
-  }
+    course
+  };
 }
 
 function mapDispatchToProps(dispatch) {
