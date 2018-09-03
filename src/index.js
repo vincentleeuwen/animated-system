31d2f309843242014654efc747a7f3d9a62d20b9
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-03 12:54:23 +0200

    fix Provider import

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
 import configureStore from './store/configureStore';
-import Provider from 'react-redux';
 import routes from './routes';
 import './styles/styles.css'; // webpack can import css too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
