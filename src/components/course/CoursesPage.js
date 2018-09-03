56d2b040b3fc530128d2a7e8830afc8ab9250197
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-03 12:53:42 +0200

    add course row for displaying courses

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -20,10 +20,15 @@ class CoursesPage extends Component {
   onClickSave() {
     this.props.dispatch(courseActions.createCourse(this.state.course));
   }
+  courseRow(course, index) {
+    return <div key={index}>{course.title}</div>;
+  }
+
   render() {
     return (
       <div>
         <h1>Courses</h1>
+        {this.props.courses.map(this.courseRow)}
         <h2>Add course</h2>
         <input
           type="text"
