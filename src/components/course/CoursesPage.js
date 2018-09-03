6d5c052930901967dc2f0d724b26604169558d74
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-03 13:24:31 +0200

    refactor matchDispatchToProps

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -18,7 +18,7 @@ class CoursesPage extends Component {
     this.setState({ course: course });
   }
   onClickSave() {
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   }
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
@@ -46,8 +46,8 @@ class CoursesPage extends Component {
 }
 
 CoursesPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  courses: PropTypes.array.isRequired
+  courses: PropTypes.array.isRequired,
+  createCourse: PropTypes.func.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -56,4 +56,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: course => dispatch(courseActions.createCourse(course))
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
