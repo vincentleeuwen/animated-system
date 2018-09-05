c8898730f2f672bce3d25f120302ceb1f8fb2af7
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 14:10:52 +0200

    add ajaxCallStatus to actions, reducer, initialState

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,8 @@
 import { createStore, applyMiddleware } from 'redux';
-import rootReducer from '../reducers';
 import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
 import thunk from 'redux-thunk';
+
+import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
   return createStore(
