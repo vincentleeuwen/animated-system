9a9cd1335c5b4e332f9d60fcbbdc35b7368dd355
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 11:24:51 +0200

    add action dispatch to index.js (should happen on load)

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import './styles/styles.css'; // webpack can import css too!
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 import { loadCourses } from './actions/courseActions';
+import { loadAuthors } from './actions/authorActions';
 
 const store = configureStore();
 store.dispatch(loadCourses());
+store.dispatch(loadAuthors());
 
 render(
   <Provider store={store}>
