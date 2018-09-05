594995de83c2d1994b0fe80501f5956abbd3e6e5
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 11:25:15 +0200

    call authors from state

--- src/components/course/ManageCoursePage.js
+++ src/components/course/ManageCoursePage.js
@@ -15,18 +15,21 @@ class ManageCoursePage extends Component {
   }
 
   render() {
+    const { course, errors } = this.state;
+    const { authors } = this.props;
     return (
       <CourseForm
-        course={this.state.course}
-        errors={this.state.errors}
-        allAuthors={[]}
+        course={course}
+        errors={errors}
+        allAuthors={authors}
       />
     );
   }
 }
 
 ManageCoursePage.propTypes = {
-  course: PropTypes.object.isRequired
+  course: PropTypes.object.isRequired,
+  authors: PropTypes.array.isRequired
 };
 
 function mapStateToProps(state, ownProps) {
@@ -38,15 +41,24 @@ function mapStateToProps(state, ownProps) {
     length: '',
     category: ''
   };
+
+  const authorsFormattedForDropdown = state.authors.map(author => {
+    return {
+      value: author.id,
+      text: `${author.firstName} ${author.lastName}`
+    };
+  });
+
   return {
-    course
+    course,
+    authors: authorsFormattedForDropdown
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     actions: bindActionCreators(courseActions, dispatch)
-  }
+  };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(ManageCoursePage);
