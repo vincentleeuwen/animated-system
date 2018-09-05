7d6c096a0dda72da1cfea7886b1ef789d9c70b09
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 12:17:09 +0200

    rewrite as arrow function

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -25,7 +25,7 @@ export function loadCourses() {
 }
 
 export function saveCourse(course) {
-  return function(dispatch, getState) {
+  return (dispatch, getState) => {
     return CourseApi.saveCourse(course).then(savedCourse => {
       course.id ? dispatch(updateCourseSuccess(savedCourse)) :
         dispatch(createCourseSuccess(savedCourse));
