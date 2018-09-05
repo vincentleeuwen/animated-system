2c154c3ff5f7920159f675f6a89633a6dc18c8be
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 12:17:55 +0200

    add add course page button & redirect

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,21 +1,32 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { browserHistory } from 'react-router';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
 
 class CoursesPage extends Component {
   constructor(props, context) {
     super(props, context);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
+  }
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
   }
   render() {
     const { courses } = this.props;
     return (
       <div>
         <h1>Courses</h1>
+        <input
+          type="submit"
+          value="Add Course"
+          className="btn btn-primary"
+          onClick={this.redirectToAddCoursePage}
+        />
         <CourseList courses={courses} />
       </div>
     );
