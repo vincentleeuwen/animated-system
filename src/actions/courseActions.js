487fc5c2874d486e996daec4958f411703bd789c
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-05 11:54:43 +0200

    saveCourse action with create / update types

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -5,12 +5,32 @@ export function loadCoursesSuccess(courses) {
   return { type: types.LOAD_COURSES_SUCCESS, courses };
 }
 
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+
+
 export function loadCourses() {
   return function(dispatch) {
     return CourseApi.getAllCourses().then(courses => {
       dispatch(loadCoursesSuccess(courses));
     }).catch(error => {
       throw(error);
+    });
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return CourseApi.saveCourse(course).then(savedCourse => {
+      course.id ? dispatch(updateCourseSuccess(savedCourse)) :
+        dispatch(createCourseSuccess(savedCourse));
+    }).catch(error => {
+      throw error;
     });
   };
 }
