130e6b2a2bf2548d50f333f125196f6a7a03a54a
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 16:19:20 +0200

    add delete course reducer

--- src/reducers/courseReducer.js
+++ src/reducers/courseReducer.js
@@ -19,6 +19,10 @@ export default function courseReducer(state = initialState.courses, action) {
         ...state.filter(course => course.id !== action.course.id),
         Object.assign({}, action.course)
       ];
+    case types.DELETE_COURSE_SUCCESS:
+      return [
+        ...state.filter(course => course.id !== action.courseId)
+      ]
     default:
       return state;
   }
