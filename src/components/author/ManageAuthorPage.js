c8a09a1e2120c1ec067a3fc31028d7accfa0e4b5
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 14:37:25 +0200

    add author form to manage author page

--- src/components/author/ManageAuthorPage.js
+++ src/components/author/ManageAuthorPage.js
@@ -1,13 +1,67 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import AuthorForm from './AuthorForm';
 
 class ManageAuthorPage extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      author: Object.assign({}, this.props.author),
+      errors: {},
+      saving: false
+    };
+
+    this.updateAuthorState = this.updateAuthorState.bind(this);
+  }
+  updateAuthorState(event) {
+    const field = event.target.name;
+    let author = Object.assign({}, this.state.author);
+    author[field] = event.target.value;
+    this.setState({ author });
+  }
+  saveAuthor(event) {
+    event.preventDefault();
+    this.context.router.push('/authors');
+  }
   render() {
+    const { saving, errors, author } = this.state;
     return (
-      <div>
-        <h1>Manage author</h1>
-      </div>
+      <AuthorForm
+        author={author}
+        errors={errors}
+        saving={saving}
+        onChange={this.updateAuthorState}
+      />
     );
   }
 }
 
-export default ManageAuthorPage;
+ManageAuthorPage.propTypes = {
+  author: PropTypes.object
+};
+
+function getAuthorById(authorId, authors) {
+  const author = authors.filter(author => author.id == authorId);
+  if (author) return author[0];
+  return null;
+}
+
+function mapStateToProps(state, ownProps) {
+  const authorId = ownProps.params.id;  // from the path `/course/:id`
+
+  let author = {
+    id: '',
+    firstName: '',
+    lastName: ''
+  };
+
+  if (authorId && state.authors > 0) {
+    author = getAuthorById(authorId);
+  }
+
+  return {
+    author
+  };
+}
+
+export default connect(mapStateToProps)(ManageAuthorPage);
