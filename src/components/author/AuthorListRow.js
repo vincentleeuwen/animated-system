3570387572b298e0ee6f894609bfe49b4857c63e
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 14:06:32 +0200

    Add link to manage author page

--- src/components/author/AuthorListRow.js
+++ src/components/author/AuthorListRow.js
@@ -1,9 +1,12 @@
 import React, { PropTypes } from 'react';
+import { Link } from 'react-router';
 
 const AuthorListRow = ({ author }) => {
   return (
     <tr>
-      <td>{author.firstName}</td>
+      <td>
+        <Link to={`/author/${author.id}`}>{author.firstName}</Link>
+      </td>
       <td>{author.lastName}</td>
       <td>{author.id}</td>
     </tr>
