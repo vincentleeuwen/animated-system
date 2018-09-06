b977e2597e542a72a4e9938112d7f83a86cbbf44
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 13:09:53 +0200

    add authors page

---
+++ src/components/authors/AuthorsPage.js
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+class AuthorsPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Authors</h1>
+      </div>
+    );
+  }
+}
+
+export default AuthorsPage;
