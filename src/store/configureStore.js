a1b25dc10c7b06946a41cf8e959b18672ff40194
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 11:11:45 +0200

    select config based on node env

---
+++ src/store/configureStore.js
@@ -0,0 +1,6 @@
+// Dynamic imports aren't supported by ES6, so we're using require instead of import
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./configureStore.prod');
+} else {
+  module.exports = require('./configureStore.dev');
+}
