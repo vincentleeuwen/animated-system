a88604c30439225a93ecab3ffbed6b4149746242
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-06 13:10:06 +0200

    add ManageAuthorPage

---
+++ src/components/authors/ManageAuthorPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ManageAuthorPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Manage author</h1>
+      </div>
+    );
+  }
+}
+
+export default ManageAuthorPage;
