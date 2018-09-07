ca7b0d2af26f9470dc7b3cb39ba5ee379484f4af
Vincent van Leeuwen <vincentleeuwen@gmail.com>
2018-09-07 13:58:09 +0200

    add number of courses to header

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,24 +1,40 @@
 import React, { PropTypes } from 'react';
 import { Link, IndexLink } from 'react-router';
+import { connect } from 'react-redux';
 import LoadingDots from './LoadingDots';
 
-const Header = ({ loading }) => {
+const Header = ({ loading, numberOfCourses }) => {
   return (
     <nav>
       <IndexLink to="/" activeClassName="active">Home</IndexLink>
       {" | "}
       <Link to="/courses" activeClassName="active">Courses</Link>
+      {
+        numberOfCourses > 0 &&
+          <span>
+            {" "}
+            <span className="badge">{numberOfCourses}</span>
+          </span>
+      }
       {" | "}
       <Link to="/authors" activeClassName="active">Authors</Link>
       {" | "}
       <Link to="/about" activeClassName="active">About</Link>
+      {" | "}
       {loading && <LoadingDots interval={200} dots={20} />}
     </nav>
   );
 };
 
 Header.propTypes = {
-  loading: PropTypes.bool.isRequired
+  loading: PropTypes.bool.isRequired,
+  numberOfCourses: PropTypes.number.isRequired
 };
 
-export default Header;
+function mapStateToProps(state, ownProps) {
+  return {
+    numberOfCourses: state.courses.length
+  };
+}
+
+export default connect(mapStateToProps)(Header);
